36f39d376c364da3b67e15d7aa0b16fa2d9d454d
DVTheWall <dhavalsanghani525@gmail.com>
2017-12-08 15:41:00 +0530

    Initial commit

---
+++ app/src/main/java/com/example/sanghani/brightcoveplayer/MainActivity.java
@@ -0,0 +1,72 @@
+package com.example.sanghani.brightcoveplayer;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.brightcove.player.edge.Catalog;
+import com.brightcove.player.edge.VideoListener;
+import com.brightcove.player.event.EventEmitter;
+import com.brightcove.player.model.Video;
+import com.brightcove.player.view.BrightcoveExoPlayerVideoView;
+
+/**
+ * This app illustrates how to use the ExoPlayer with the Brightcove
+ * Native Player SDK for Android.
+ *
+ * @author Billy Hnath (bhnath@brightcove.com)
+ */
+public class MainActivity extends AppCompatActivity {
+
+    private final String TAG = this.getClass().getSimpleName();
+
+    BrightcoveExoPlayerVideoView brightcoveVideoView;
+    Catalog catalog;
+    Button btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // When extending the BrightcovePlayer, we must assign the brightcoveVideoView before
+        // entering the superclass. This allows for some stock video player lifecycle
+        // management.  Establish the video object and use it's event emitter to get important
+        // notifications and to control logging.
+        setContentView(R.layout.activity_main);
+        brightcoveVideoView = (BrightcoveExoPlayerVideoView) findViewById(R.id.brightcove_video_view);
+        btn = findViewById(R.id.btn);
+        super.onCreate(savedInstanceState);
+
+        // Get the event emitter from the SDK and create a catalog request to fetch a video from the
+        // Brightcove Edge service, given a video id, an account id and a policy key.
+        EventEmitter eventEmitter = brightcoveVideoView.getEventEmitter();
+        catalog = new Catalog(eventEmitter, getString(R.string.account), getString(R.string.policy));
+
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                playvideo();
+
+            }
+        });
+
+    }
+
+    private void playvideo() {
+
+        catalog.findVideoByID(getString(R.string.videoId), new VideoListener() {
+
+            // Add the video found to the queue with add().
+            // Start playback of the video with start().
+            @Override
+            public void onVideo(Video video) {
+                Log.v(TAG, "onVideo: video = " + video);
+                brightcoveVideoView.add(video);
+                brightcoveVideoView.start();
+            }
+        });
+    }
+
+}
